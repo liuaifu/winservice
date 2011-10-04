0d6f2862c60dea69dc0a2bc9e0453edaf90fd272
laf163 <laf163@localhost.localdomain>
2011-10-04 12:54:59 +0800

    fix cgo free can't compile problem

--- winservice.go
+++ winservice.go
@@ -5,14 +5,14 @@
 package winservice
 
 // #include "winservice.h"
+// #include <stdlib.h>
+// #include <memory.h>
 import "C"
-
-//import "unsafe"
-
+import "unsafe"
 func Start(name string) {
 	p := C.CString(name)
 	C.start(p)
-	//C.free(unsafe.Pointer(p))
+	C.free(unsafe.Pointer(p))
 }
 
 func Stop() {
